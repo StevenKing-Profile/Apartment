e6f2c3b1ec37327d0d70f02a0c9b323dfdd12bc9
Steven King <nfstevenking@gmail.com>
2019-06-19 22:38:01 -0400

    Adding chromedriver.exe for version 75 because chrome 76 isn't a thing yet....

--- apartment-api/src/main/java/net/stevenking/apartment/service/SeleniumService.java
+++ apartment-api/src/main/java/net/stevenking/apartment/service/SeleniumService.java
@@ -3,10 +3,9 @@ package net.stevenking.apartment.service;
 import net.stevenking.apartment.data.Apartment;
 import net.stevenking.apartment.data.FloorPlan;
 import net.stevenking.apartment.repository.FloorPlanRepository;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SeleniumService {
@@ -24,10 +25,16 @@ public class SeleniumService {
 
     public void fetchNewPrice() throws MalformedURLException {
         List<FloorPlan> floorPlanList = floorPlanRepository.findAll();
-        URL local = new URL("http://localhost:9515");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        Capabilities cap = DesiredCapabilities.chrome();
+        ((DesiredCapabilities) cap).setPlatform(org.openqa.selenium.Platform.WINDOWS);
         for (FloorPlan fp : floorPlanList) {
             Apartment apartment = new Apartment();
-            WebDriver driver = new RemoteWebDriver(local, DesiredCapabilities.chrome());
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setExperimentalOption("excludeSwitches", Arrays.asList("test-type"));
+            WebDriver driver = new ChromeDriver(chromeOptions);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
             driver.get(fp.getUrl());
             WebElement element = driver.findElement(By.cssSelector("[data-selenium-id=\"Rent1\"]"));
             apartment.setPrice(Short.valueOf(element.getText()));
